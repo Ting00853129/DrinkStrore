da277d5feebbfd6a0cf0821f19cc8ec758030e21
Ting <yitingwu.1030@gmail.com>
2022-10-23 09:43:06 +0800

    Initial Commit

---
+++ DrinkStrore/DrinkStroreApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkStroreApp.swift
+//  DrinkStrore
+//
+//  Created by Victor Tao on 2022/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DrinkStroreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
